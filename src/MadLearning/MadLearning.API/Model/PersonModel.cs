c7b9b4e9cf3b896182733eb1b719ad70e8881d05
Raymond Selvik <raymond.selvik@itera.no>
2020-09-21 11:10:12 +0200

    Added API for creating, retrieving, updating and deleting events.

---
+++ src/MadLearning/MadLearning.API/Model/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MadLearning.API.Model
+{
+    public class PersonModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ src/MadLearning/MadLearning.API/Service/IEventRepository.cs
@@ -0,0 +1,17 @@
+﻿using MadLearning.API.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MadLearning.API.Service
+{
+    public interface IEventRepository
+    {
+        Task<List<EventModel>> GetEvents(EventFilter eventFilter);
+
+        Task<EventModel> CreateEvent(EventModel eventModel);
+
+        Task DeleteEvent(EventModel eventModel);
+
+        Task UpdateEvent(EventModel eventModel);
+    }
+}
