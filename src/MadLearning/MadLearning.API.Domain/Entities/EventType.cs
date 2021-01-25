53ad4a580563c7b82a8b041531cf211d43cc3988
duytantran <duytan95@hotmail.com>
2021-01-25 20:50:51 +0100

    add EventRepository file

---
+++ src/MadLearning/MadLearning.API.Domain/Entities/EventType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLearning.API.Domain.Entities
+{
+    public enum EventType
+    {
+        SubjectMatterEvent,
+        CodingEvent,
+        ProjectPresentation,
+        Workshop,
+    }
+
+    public static class ToEventTypeEnum
+    {
+        public static EventType ToEventTypeNum(string eventType)
+        {
+            return eventType switch
+            {
+                "Subject matter event" => EventType.SubjectMatterEvent,
+                "Coding event" => EventType.CodingEvent,
+                "Project presentation" => EventType.ProjectPresentation,
+                "Workshop" => EventType.Workshop,
+                _ => throw new Exception(),
+            };
+        }
+    }
+}
